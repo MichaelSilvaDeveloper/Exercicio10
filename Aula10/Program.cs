2275233aa1012f662c8fa6808c41ddfa5bd6b964
Michael <Michael@DESKTOP-61L2M0C>
2023-10-16 19:50:16 -0300

    Adicionar arquivos de projeto.

---
+++ Aula10/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula10
+{
+    class Program
+    {
+        enum DiasSemana { Domingo, Segunda, Terça, Quarta, Quinta, Sexta, Sábado }
+        static void Main(string[] args)
+        {
+            //DiasSemana ds = DiasSemana.Domingo;
+            int ds = (int)DiasSemana.Sexta;
+            Console.WriteLine(ds);
+        }
+    }
+}
